bc63d0b089e37b4d3d4e9c155b45eeb264cb5c6f
KevinAp-5 <keven.clash3@gmail.com>
2023-08-22 23:30:34 -0300

    replaced tabs with whitespaces

--- goalScanner.js
+++ goalScanner.js
@@ -1,13 +1,13 @@
 const interval = setInterval(function() {
-	const gol_eye = document.getElementsByClassName('ovm-GoalEventAlert_Text');
+    const gol_eye = document.getElementsByClassName('ovm-GoalEventAlert_Text');
 
     if (gol_eye.length > 0) {
-		clearInterval(interval);
+        clearInterval(interval);
 
         let times_gol = gol_eye[0];  // Get the match div
-		for (let i = 0; i < 3; i++) {
-			times_gol = times_gol.parentElement;
-		}
+        for (let i = 0; i < 3; i++) {
+            times_gol = times_gol.parentElement;
+        }
 
     }
 
