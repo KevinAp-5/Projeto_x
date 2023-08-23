4ea162031013b0605844daa7315c87ceeeebc856
KevinAp-5 <keven.clash3@gmail.com>
2023-08-22 23:25:03 -0300

    verify if a goal was made

--- goalScanner.js
+++ goalScanner.js
@@ -1,3 +1,9 @@
 const interval = setInterval(function() {
 	const gol_eye = document.getElementsByClassName('ovm-GoalEventAlert_Text');
+
+    if (gol_eye.length > 0) {
+		clearInterval(interval);
+
+    }
+
 }, 1000);
