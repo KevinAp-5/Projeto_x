8f8ac6680973062e4de5b2215c17aeb7eb80d2df
KevinAp-5 <keven.clash3@gmail.com>
2023-08-24 20:29:23 -0300

    exist_position()

--- mouse.py
+++ mouse.py
@@ -7,3 +7,10 @@ class FileManagement:
         self.home = os.path.expanduser('~')
         self.downloads = self.home + '/Downloads'
         self.can_delete = False
+
+    def exist_position(self):
+        if os.path.exists(f'{self.downloads}/position.json'):
+            self.can_delete = False
+            return True
+        else:
+            return False
