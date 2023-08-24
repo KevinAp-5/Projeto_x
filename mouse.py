71510e47465f8d5d23049b90d70601d25cf392a5
KevinAp-5 <keven.clash3@gmail.com>
2023-08-24 20:29:07 -0300

    class FIlemanagement

--- mouse.py
+++ mouse.py
@@ -2,3 +2,8 @@ from json import load
 import os
 import pyautogui
 
+class FileManagement:
+    def __init__(self):
+        self.home = os.path.expanduser('~')
+        self.downloads = self.home + '/Downloads'
+        self.can_delete = False
