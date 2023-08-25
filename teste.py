cfc4a209f52125f0277185962a9610c4d8d4fe59
KevinAp-5 <keven.clash3@gmail.com>
2023-08-25 17:43:36 -0300

    teste.py

---
+++ teste.py
@@ -0,0 +1,14 @@
+from typing import Optional
+from ctypes import wintypes, windll, create_unicode_buffer
+
+def getForegroundWindowTitle() -> Optional[str]:
+    hWnd = windll.user32.GetForegroundWindow()
+    length = windll.user32.GetWindowTextLengthW(hWnd)
+    buf = create_unicode_buffer(length + 1)
+    windll.user32.GetWindowTextW(hWnd, buf, length + 1)
+    
+    # 1-liner alternative: return buf.value if buf.value else None
+    if buf.value:
+        return buf.value
+    else:
+        return None
