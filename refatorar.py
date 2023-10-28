3896a27209f88a4618f54a06053c5c0c5ada86f7
KevinAp-5 <keven.clash3@gmail.com>
2023-10-28 03:10:32 -0300

    clean the files when come back from a pause
    When you come back from a pause, it may have a file because a goal was found, and it will release the click functions to go the goal, but it is an old goal, and clean the file fix it

--- refatorar.py
+++ refatorar.py
@@ -213,6 +213,7 @@ class MoveMouse:
                 except KeyboardInterrupt:
                     print()
                     Keyboard().treat_input()
+                    FileManager().delete_position()
                 counter += 0.2
             else:
                 Printer.clean()
