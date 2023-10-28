bc7df72379dc6218b5687ad1ae847b806a122766
KevinAp-5 <keven.clash3@gmail.com>
2023-10-28 03:34:59 -0300

    applying new changes to the mouse.py file

--- mouse.py
+++ mouse.py
@@ -1,3 +1,4 @@
+from clipboard import copy
 from json import load
 import os
 from os import get_terminal_size
@@ -5,157 +6,21 @@ import pyautogui
 from sys import platform, argv
 from glob import glob
 from time import sleep
-from pyperclip import copy
-from plyer import notification
+from playsound import playsound
 
+WINDOWS = False
 if 'win' in platform:
     from ctypes import wintypes, windll, create_unicode_buffer
-#    from pywinauto import Desktop, Aplication
+    WINDOWS = True
+
+
+def terminal_size():
+    return int(get_terminal_size()[0])
 
 
 def get_argv():
     conf = [int(float(item.strip())) for item in argv[1:]]
     return conf[:3]
-
-
-def printer(text):
-    print(str(text).center(terminal_size()), end='\r', flush=True)
-
-
-class FileManager:
-    def __init__(self):
-        self.home = os.path.expanduser('~')
-        self.path = self.home + '/Downloads'
-        self.can_delete = False
-
-    def exist_position(self):
-        if os.path.exists(f'{self.path}/position.json'):
-            self.can_delete = False
-            return True
-        else:
-            return False
-
-    def get_json(self):
-        position = dict()
-        with open(f'{self.path}/position.json', 'r+') as position_json:
-            position = load(position_json)
-            printer('Procurando')
-            self.can_delete = True
-            position_json.close()
-        return position
-
-    def delete_position(self):
-        if self.can_delete is True:
-            for file in glob(f'{self.path}/position*.json'):
-                os.remove(file)
-            return True
-        else:
-            return False
-
-    def get_position(self):
-        if self.exist_position() is True:
-            my_position = self.get_json()
-            self.delete_position()
-            return my_position
-        else:
-            return False
-
-
-class MoveMouse:
-    def __init__(self):
-        pyautogui.PAUSE = 0.01
-        manager = FileManager()
-        self.manager = manager
-        self.counter = 0
-
-    def write(self, text):
-        copy(text)
-        sleep(0.1)
-        pyautogui.hotkey('ctrl', 'v')
-
-    def get_both(self, x_y=True, full=False):
-        pos_dict = self.manager.get_position()
-        if pos_dict is False:
-            return False
-        else:
-            pos_dict = list(pos_dict.items())
-
-        if full is True:
-            return dict(pos_dict)
-
-        if x_y is True:  # return x, y
-            pos_dict = pos_dict[:2]
-        else:  # return width, height
-            pos_dict = pos_dict[2:]
-
-        x, y = pos_dict[0][1], pos_dict[1][1]
-        return x, y
-
-    def move(self, x, y):
-        filterr = argv[0]
-        if 'mouse' in filterr:
-            filterr = 2
-        else:
-            filterr = int(argv[0])
-
-        a = self.get_both(full=True)
-        if a is False:
-            return False
-
-        json_info = list(a.values())
-        x, y = json_info[0], json_info[1]
-        w, h = json_info[2], json_info[3]
-        nome = json_info[4]
-
-        if 'win' in platform:
-            while 'bet' not in WindowManager().window_title():
-                text = 'aguardando voltar para janela da bet'
-                for x in range(3):
-                    printer(str(text+'.'*x))
-                    sleep(0.5)
-                    if 'bet' in WindowManager.window_title():
-                        break
-                sleep(0.1)
-        print()
-
-        copy('')
-        pyautogui.moveTo(1107)
-        pyautogui.moveTo(y=575)
-        pyautogui.hotkey('esc')
-        pyautogui.hotkey('f3')
-        partida = ' '.join(nome.split(' ')[:filterr])
-
-        a = ' '.join(partida.split('GOL'))
-        b = a.split(' ')[0]
-        if len(b) <= 5:
-            partida = a
-        else:
-            partida = b
-#        notification.notify(title=partida, timeout=3)
-        partida = ''.join(partida)
-        printer(partida)
-        print()
-        self.counter += 1
-        printer(f"Gols encontrados: {self.counter}")
-        print()
-        print()
-        self.write(partida)
-        sleep(0.1)
-        pyautogui.click()
-        sleep(0.009)
-        pyautogui.click()
-        try:
-            sleep(5)
-        except KeyboardInterrupt:
-            exit()
-        pyautogui.hotkey('f3')
-        pyautogui.hotkey('backspace')
-        sleep(0.1)
-        pyautogui.hotkey('esc')
-
-
-def terminal_size():
-    return get_terminal_size()[0]
 
 
 def banner(title):
@@ -177,38 +42,231 @@ def nice_line():
             sleep_time = 0
 
 
-class Flow:
+class Get_input():
     def __init__(self):
-        print(banner('Mineirinho Scanner'))
-        nice_line()
-        filer = FileManager()
-        filer.exist_position()
-        filer.delete_position()
+        self.text = 'Você quer continuar? [S/n]\n'
 
-    def main(self):
-        mouse = MoveMouse()
-        i = 0
-        x, y = 0, 0
+    def get_answer(self):
         while True:
-            i = mouse.get_both()
-
-            if type(i) != bool:
-                x, y = i[0], i[1]
-                break
-            else:
-                printer('aguardando gol')
-                try:
-                    sleep(0.1)
-                except KeyboardInterrupt:
-                    exit()
-
-        while True:
-            mouse.move(x, y)
             try:
-                sleep(0.1)
+                Printer(self.text)
+                x = int((terminal_size()/2)-1)
+                self.text = input(' '*x)
+                self.text = self.text.strip().lower()[0]
             except KeyboardInterrupt:
                 exit()
-        mouse.position_update()
+            except IndexError:
+                continue
+
+            if self.text:
+                return self.text
+
+
+class Keyboard():
+    def __init__(self):
+        self.user_input = Get_input()
+
+    def treat_input(self):
+        while True:
+            resume_pomodoro = self.user_input.get_answer()
+            if resume_pomodoro == 's':
+                Printer('Continuando.')
+                break
+            elif resume_pomodoro == 'n':
+                Printer('\nAdeus!')
+                exit()
+            else:
+                Printer('Resposta inválida! use [S/N]\n')
+                self.reset()
+                continue
+        self.reset()
+
+    def reset(self):
+        self.user_input = Get_input()
+
+
+class Printer:
+    def __init__(self, text):
+        print(str(text).center(terminal_size()), end='\r', flush=True)
+
+    def clean():
+        Printer(' '*terminal_size())
+
+
+class FileManager:
+    def __init__(self):
+        self.home = os.path.expanduser('~')
+        self.path = self.home + '/Downloads'
+        self.can_delete = False
+
+    def file_exists(self):
+        return os.path.exists(f'{self.path}/position.json')
+    def get_json(self):
+        position = dict()
+
+        if self.file_exists() is False:
+            return False
+        to_open = f'{self.path}/position.json'
+
+        with open(to_open, 'r+', encoding='utf-8') as position_json:
+            position = load(position_json)
+            self.can_delete = True
+            position_json.close()
+        return position
+
+    def delete_position(self):
+        for file in glob(f'{self.path}/position*.json'):
+            os.remove(file)
+        return True
+
+    def get_nome(self, json_info):
+        return list(json_info.values())[4]
+
+
+def fix_sub(lista):
+    new_list = list()
+
+    def remove_digit(name):
+        new_name = str()
+        for letter in name:
+            if letter.isdigit() is False:
+                new_name += letter
+        return new_name
+
+    for name in lista:
+        if 'sub' in name.lower():
+            name = remove_digit(name.replace('Sub-', ''))
+        new_list.append(name)
+    return new_list
+
+
+def esport_fix(name_list):
+    new_name = list()
+    for x in name_list:
+        x = x.replace('Esports', '')
+        if x != '':
+            new_name.append(x)
+    return fix_sub(new_name)
+
+
+def bad_split(times):
+    nome = times[-1]
+    last_two = times[-2:]
+    if len(nome) <= 2 or '(' in nome:
+        nome = last_two
+    elif len(nome) >= 3:
+        if len([x for x in nome if x.upper()]) <= 1 or len(times[-2]) == 2:
+            nome = last_two
+
+    if isinstance(nome, list):
+        nome = ' '.join(nome)
+
+    return nome
+
+
+def team_goal(raw_info):
+    times = raw_info.split('GOL')
+    if times[-1] == '':  # BAD SPLIT
+        times.pop(-1)
+        times = ''.join(times).split(' ')
+        times = esport_fix(times)
+        times = bad_split(times)
+    elif len(times) == 2:  # GOOD SPLIT
+        times = esport_fix(times)
+        times = times[0].strip()
+
+    return times
+
+
+class MoveMouse:
+    def __init__(self):
+        pyautogui.PAUSE = 0.01
+        manager = FileManager()
+        self.manager = manager
+        self.seconds = 0
+
+    def write(self, text):
+        copy(text)
+        sleep(0.2)
+        pyautogui.hotkey('ctrl', 'v')
+        return True
+
+    def clean_clipboard(self):
+        copy('')
+
+    def partida_name(self, gol_info):
+        nome = team_goal(gol_info)
+        self.manager.delete_position()
+        return nome.lower()
+
+    def open_search(self):
+        self.clean_clipboard()
+        sleep(0.3)
+        pyautogui.hotkey('esc')
+        pyautogui.hotkey('f3')
+        return True
+
+    def click(self):
+        sleep(0.1)
+        pyautogui.click()
+
+    def clean_search(self):
+        sleep(1.5)
+        pyautogui.hotkey('f3')
+        pyautogui.hotkey('backspace')
+        sleep(0.1)
+        pyautogui.hotkey('esc')
+
+    def move_mouse(self, x=1270, y=570):
+        pyautogui.moveTo(x)
+        pyautogui.moveTo(y=y)
+
+    def main(self, x=1270, y=570):
+        x = 1270
+        y = 580
+
+        if WINDOWS is True:
+            WindowManager.bet_loop()
+
+        nome_time = self.partida_name(self.gol_info_loop())
+        Printer(f'{nome_time} - {self.seconds:.2f}s')
+        print()
+        print()
+
+        playsound('sound.mp3')
+        opened = self.open_search()
+        sleep(0.1)
+        self.move_mouse(x, y)
+        can_click = self.write(nome_time)
+        if opened is True and can_click is True:
+            sleep(1)
+        else:
+            print('False')
+            sleep(1.5)
+        self.click()
+        self.clean_search()
+
+    def gol_info_loop(self):
+        counter = 0
+        while True:
+            alert = ' - Espera alta, verifique o site ou programa'
+            gol_info = self.manager.get_json()
+            if gol_info is False:
+                if counter <= 200:
+                    alert = ''
+                Printer(f'aguardando gol {counter:.2f}s {alert}')
+                try:
+                    sleep(0.2)
+                except KeyboardInterrupt:
+                    print()
+                    Keyboard().treat_input()
+                    FileManager().delete_position()
+                counter += 0.2
+            else:
+                Printer.clean()
+                nome = self.manager.get_nome(gol_info)
+                self.seconds = counter
+                return nome
 
 
 class WindowManager:
@@ -223,8 +281,43 @@ class WindowManager:
         else:
             return ''
 
+    def check_tab_name(self):
+        tab_name = WindowManager().window_title()
+        x = tab_name.split(' ')[0].lower()
+        if 'bet' in x:
+            return True
+        else:
+            return False
+
+    def bet_loop(self):
+        while True:
+            if self.check_tab_name() is False:
+                print('aguardando voltar para bet', '\r', flush=True)
+            else:
+                break
+            try:
+                sleep(0.2)
+            except KeyboardInterrupt:
+                print()
+                Keyboard().treat_input()
+
+
+class Main:
+    def __init__(self):
+        print(banner('Mineirinho Scanner'))
+        nice_line()
+
+    def run(self):
+        FileManager().delete_position()
+        sleep(0.5)
+
+        try:
+            while True:
+                MoveMouse().main()
+        except KeyboardInterrupt:
+            print()
+            Keyboard().treat_input()
+
 
 if __name__ == '__main__':
-    x = Flow()
-    x.main()
-    #x.move()
+    Main().run()
